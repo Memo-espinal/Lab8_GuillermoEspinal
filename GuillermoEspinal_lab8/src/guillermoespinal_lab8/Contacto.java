4753cfdc10e180fac07028ad723cc2520bc09cea
Guillermo Espinal <guille.gaes@gmail.com>
2019-12-06 13:37:40 -0600

    First commit hoy si

---
+++ GuillermoEspinal_lab8/src/guillermoespinal_lab8/Contacto.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guillermoespinal_lab8;
+
+/**
+ *
+ * @author admin
+ */
+//. Los contactos tienen nombre, edad, número telefónico, correo electrónico y dirección.
+public class Contacto {
+    private  String nombre;
+    private int edad;
+    private  String numero ;
+    private String correo;
+    private String dirrecion;
+    
+    
+}
